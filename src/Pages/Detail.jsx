30c07bb873f040b29bdedeec9dae20357c83c088
hanep <yudishan26@gmail.com>
2024-06-15 14:37:45 +0700

    update quiz detail page

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,92 +1,27 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
+import axiosClient from "../axios"
 
 export default function Detail() {
-    const data = {
-        id: 1,
-        title: "javascript quizz for beginner",
-        description: "you can test your javascript knowledge by attempting to this quizz",
-        category: "programming",
-        difficulity: "easy",
-        author: {
-            id: 1,
-            name: "steve",
-            email: "steve@gmail.com"
-        },
-        questions: [
-            {
-                question: "what is the output of this program",
-                answers: [
-                    {
-                        answer: "null",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "1",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "undefined",
-                        is_correct_answer: true
-                    },
-                    {
-                        answer: "true",
-                        is_correct_answer: false
-                    }
-                ]
-            },
-            {
-                question: "what is the output of this program",
-                answers: [
-                    {
-                        answer: "null",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "1",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "undefined",
-                        is_correct_answer: true
-                    },
-                    {
-                        answer: "true",
-                        is_correct_answer: false
-                    }
-                ]
-            },
-            {
-                question: "what is the output of this program",
-                answers: [
-                    {
-                        answer: "null",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "1",
-                        is_correct_answer: false
-                    },
-                    {
-                        answer: "undefined",
-                        is_correct_answer: true
-                    },
-                    {
-                        answer: "true",
-                        is_correct_answer: false
-                    }
-                ]
-            }
-        ]
-    }
+    const [data, setData] = useState({})
+    const { pathname } = useLocation()
+    const quiz_id = pathname.split('/')[3]
+
+    useEffect(() => {
+        axiosClient.get('/quizzes/' + quiz_id).then(response => {
+            setData(response.data)
+            console.log(response.data)
+        })
+    }, [])
 
     return (
         <div className="bg-slate-700 p-4 rounded">
             <h2 className="text-xl md:text-2xl font-semibold mb-3">{data.title}</h2>
             <div className="mb-3 text-slate-300 font-light">
-                <p>difficulity : <span className={`${data.difficulity == 'easy' && 'text-green-500'} ${data.difficulity == 'medium' && 'text-yellow-500'} ${data.difficulity == 'hard' && 'text-red-500'} font-medium`}>{data.difficulity}</span></p>
-                <p>category : <span className="text-blue-400 font-medium">{data.category}</span></p>
-                <p>author : <Link to={'/users/' + data.author.id} className="text-slate-200 font-medium">{data.author.name}</Link></p>
-                <p>number of questions : <span className="text-slate-200 font-medium">{data.questions.length}</span></p>
+                <p>difficulity : <span className={`${data?.difficulity?.slug == 'easy' && 'text-green-500'} ${data?.difficulity?.slug == 'medium' && 'text-yellow-500'} ${data?.difficulity?.slug == 'hard' && 'text-red-500'} font-medium`}>{data?.difficulity?.title}</span></p>
+                <p>category : <span className="text-blue-400 font-medium">{data?.category?.title}</span></p>
+                <p>author : <Link to={'/users/' + data?.author?.id} className="text-slate-200 font-medium">{data?.author?.name}</Link></p>
+                <p>number of questions : <span className="text-slate-200 font-medium">{data?.questions?.length}</span></p>
             </div>
             <p className="mb-3 text-slate-300 font-light">{data.description}</p>
             <div className="flex justify-end gap-2 text-white">
