d348d9c73614042f1e3a545bfcd12bf1d997c696
hanep <yudishan26@gmail.com>
2024-06-22 22:11:02 +0700

    update create quiz page(1)

--- src/Pages/CreateQuiz.jsx
+++ src/Pages/CreateQuiz.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axiosClient from "../axios"
 
 export default function CreateQuiz() {
     const [questions, setQuestions] = useState([{ index: 1, content: "", answers: [] }])
+    const [categories, setCategories] = useState([])
+    const [difficulities, setDifficulities] = useState([])
 
     const handleAddQuestionNumber = () => {
         setQuestions([...questions, { index: questions.length + 1, content: "", answers: [] }])
@@ -12,8 +15,36 @@ export default function CreateQuiz() {
         setQuestions(questions.slice(0, questions.length - 1))
     }
 
+    useEffect(() => {
+        axiosClient.get('/categories').then(response => setCategories(response.data))
+        axiosClient.get('/difficulities').then(response => setDifficulities(response.data))
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!confirm("are you sure?")) return
+
+        const data = {
+            title: e.target.title.value,
+            description: e.target.description.value,
+            category_id: e.target.category_id.value,
+            difficulity_id: e.target.difficulity_id.value,
+            questions: questions
+        }
+
+        console.log(data)
+    }
+
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="title">Quiz Title</label>
+                <input type="text" id="title" className="bg-slate-700 h-8 px-2 rounded w-full outline-none" name="title" placeholder="title" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="description">Description</label>
+                <textarea id="description" className="bg-slate-700 px-2 py-1 rounded w-full outline-none" name="description" placeholder="description" />
+            </div>
             <div className="mb-3 flex flex-col sm:flex-row gap-3">
                 <div className="flex sm:flex-col gap-2 sm:gap-1 items-center sm:items-start">
                     <label htmlFor="numberOfQuestion">Number of Question</label>
@@ -25,29 +56,39 @@ export default function CreateQuiz() {
                 </div>
                 <div className="flex sm:flex-col gap-2 sm:gap-1 items-center sm:items-start">
                     <label htmlFor="category">Category</label>
-                    <select name={"category"} id={"category_id"} className="bg-slate-700 px-2 border-none h-8 outline-none rounded border border-slate-500">
-                        <option>{"title"}</option>
-                        {/* {list?.map(value => <option key={value.slug} value={value.slug}>{value.title}</option>)} */}
+                    <select name={"category_id"} id={"category_id"} className="bg-slate-700 px-2 border-none h-8 outline-none rounded border border-slate-500">
+                        {categories?.map(value => <option key={value.slug} value={value.slug}>{value.title}</option>)}
                     </select>
                 </div>
                 <div className="flex sm:flex-col gap-2 sm:gap-1 items-center sm:items-start">
                     <label htmlFor="difficulity">Difficulity</label>
-                    <select name={"difficulity"} id={"difficulity_id"} className="bg-slate-700 px-2 border-none h-8 outline-none rounded border border-slate-500">
-                        <option>{"title"}</option>
-                        {/* {list?.map(value => <option key={value.slug} value={value.slug}>{value.title}</option>)} */}
+                    <select name={"difficulity_id"} id={"difficulity_id"} className="bg-slate-700 px-2 border-none h-8 outline-none rounded border border-slate-500">
+                        {difficulities?.map(value => <option key={value.slug} value={value.slug}>{value.title}</option>)}
                     </select>
                 </div>
             </div>
-            {questions.map(question => <Question key={question.index} />)}
+            {questions.map(question => <Question key={question.index} question={question} questions={questions} setQuestions={setQuestions} />)}
+            <div className="mb-3">
+                <button className="bg-green-600 hover:bg-green-500 h-8 px-5 rounded">Save</button>
+            </div>
         </form>
     )
 }
 
-function Question() {
-    const [answers, setAnswers] = useState([{ index: 1, content: "" }, { index: 2, content: "" }, { index: 3, content: "" }, { index: 4, content: "" }])
+function Question({ question, questions, setQuestions }) {
+    const [answers, setAnswers] = useState([{ index: 1, content: "", is_correct: true }, { index: 2, content: "", is_correct: false }, { index: 3, content: "", is_correct: false }, { index: 4, content: "", is_correct: false }])
+
+    useEffect(() => {
+        setQuestions(questions.map(qst => {
+            if (qst.index == question.index) {
+                qst.answers = answers
+            }
+            return qst
+        }))
+    }, [answers])
 
     const handleAddAnswer = () => {
-        setAnswers([...answers, { index: answers.length + 1, content: "" }])
+        setAnswers([...answers, { index: answers.length + 1, content: "", is_correct: false }])
     }
 
     const handleReduceAnswer = () => {
@@ -55,35 +96,42 @@ function Question() {
         setAnswers(answers.slice(0, answers.length - 1))
     }
 
+    const handleCorrectAnswer = (e) => {
+        setAnswers(answers.map(answer => {
+            answer.is_correct = answer.index == e.target.value
+            return answer
+        }))
+    }
+
+    const handleChangeAnswer = (e) => {
+        setAnswers(answers.map(answer => {
+            if (answer.index == e.target.dataset.index) {
+                answer.content = e.target.value
+            }
+            return answer
+        }))
+    }
+
+    const handleQuestionTitle = (e) => {
+        setQuestions(questions.map(qst => {
+            if (qst.index == question.index) {
+                qst.content = e.target.value
+            }
+            return qst
+        }))
+    }
+
     return (
         <div className="bg-slate-700 p-4 rounded mb-3">
             <div className="mb-3 flex flex-col gap-1">
                 <label htmlFor="question">Question</label>
-                <textarea type="text" id="question" name="question" placeholder="Question..." className="w-full bg-transparent backdrop-brightness-125 px-2 py-1 rounded outline-none" rows={3} />
-            </div>
-            <div className="grid grid-cols-2 gap-3">
-                <div className="mb-3 flex flex-col gap-1">
-                    <label htmlFor="answer">Category</label>
-                    <select name="right_answer" id="right_answer" className="bg-transparent backdrop-brightness-125 outline-none p-2 rounded">
-                        <option value="1">Math</option>
-                        <option value="2">Programming</option>
-                        <option value="3">Sport</option>
-                    </select>
-                </div>
-                <div className="mb-3 flex flex-col gap-1">
-                    <label htmlFor="answer">Difficulity</label>
-                    <select name="right_answer" id="right_answer" className="bg-transparent backdrop-brightness-125 outline-none p-2 rounded">
-                        <option value="1">Easy</option>
-                        <option value="2">Medium</option>
-                        <option value="3">Hard</option>
-                    </select>
-                </div>
+                <textarea type="text" id="question" name="question" onKeyUp={handleQuestionTitle} placeholder="Question..." className="w-full bg-transparent backdrop-brightness-125 px-2 py-1 rounded outline-none" rows={3} />
             </div>
             <div className="mb-3 flex flex-col gap-1">
                 <label htmlFor="answer">answer</label>
                 <div className="flex flex-col gap-3">
                     {answers.map(answer => (
-                        <input key={answer.index} type="text" id="answer" name="answer" placeholder={answer.index + ". answer..."} className="w-full bg-transparent backdrop-brightness-125 p-2 rounded outline-none" />
+                        <input onChange={handleChangeAnswer} key={answer.index} data-index={answer.index} type="text" id="answer" name="answer" placeholder={answer.index + ". answer..."} className="w-full bg-transparent backdrop-brightness-125 p-2 rounded outline-none" autoComplete="off" />
                     ))}
                     <div className="grid grid-cols-2 gap-3">
                         <button type="button" onClick={handleAddAnswer} className="w-full bg-transparent backdrop-brightness-125 hover:bg-slate-600 active:bg-slate-500 p-2 rounded outline-none">+ Add Answer</button>
@@ -92,15 +140,12 @@ function Question() {
                 </div>
             </div>
             <div className="mb-3 flex flex-col gap-1">
-                <label htmlFor="answer">Right Answer</label>
-                <select name="right_answer" id="right_answer" className="bg-transparent backdrop-brightness-125 outline-none p-2 rounded">
+                <label htmlFor="correct_answer">Correct Answer</label>
+                <select name="correct_answer" id="correct_answer" onChange={handleCorrectAnswer} className="bg-transparent backdrop-brightness-125 outline-none p-2 rounded">
                     {answers.map(answer => (
                         <option key={answer.index} className="bg-slate-600" value={answer.index}>{answer.index + " - " + answer.content}</option>
                     ))}
                 </select>
-            </div>
-            <div className="mb-3">
-                <button className="bg-green-600 hover:bg-green-500 h-8 px-5 rounded">Save</button>
             </div>
         </div>
     )
